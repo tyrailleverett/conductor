66af1d1022f7eac9668dec82f8b8a4bd9852f632
Tyrail Leverett <tyrailleverett@gmail.com>
2026-03-07 22:41:13 -0600

    Phase 1.2: Configuration File

---
+++ config/conductor.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'path' => env('CONDUCTOR_PATH', 'conductor'),
+
+    'middleware' => ['web'],
+
+    'queue' => [
+        'connection' => env('CONDUCTOR_QUEUE_CONNECTION'),
+        'queue' => env('CONDUCTOR_QUEUE', 'conductor'),
+    ],
+
+    'prune_after_days' => (int) env('CONDUCTOR_PRUNE_AFTER_DAYS', 7),
+
+    'heartbeat_interval' => (int) env('CONDUCTOR_HEARTBEAT_INTERVAL', 15),
+
+    'worker_timeout' => (int) env('CONDUCTOR_WORKER_TIMEOUT', 60),
+
+    'redact_keys' => [
+        'password',
+        'token',
+        'authorization',
+        'secret',
+        'api_key',
+        'cookie',
+        'x-signature',
+        'x-hub-signature',
+    ],
+
+    'functions' => [],
+
+    /*
+    'webhooks' => [
+        'stripe' => [
+            'secret' => env('CONDUCTOR_WEBHOOK_STRIPE_SECRET'),
+            'function' => 'billing.handle-stripe-webhook',
+        ],
+    ],
+    */
+    'webhooks' => [],
+
+    'webhook_rate_limit' => env('CONDUCTOR_WEBHOOK_RATE_LIMIT', 60),
+];
