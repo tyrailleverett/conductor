402a73f0594e7a8998c0c82cd0a986dd3f19d5b6
Tyrail Leverett <tyrailleverett@gmail.com>
2026-03-07 23:13:32 -0600

    Phase 1.3: Conductor Main Class

--- src/Conductor.php
+++ src/Conductor.php
@@ -4,4 +4,24 @@ declare(strict_types=1);
 
 namespace HotReloadStudios\Conductor;
 
-final class Conductor {}
+use Closure;
+use Illuminate\Http\Request;
+
+final class Conductor
+{
+    private static ?Closure $authUsing = null;
+
+    public static function auth(Closure $callback): void
+    {
+        self::$authUsing = $callback;
+    }
+
+    public static function check(Request $request): bool
+    {
+        if (self::$authUsing instanceof Closure) {
+            return (bool) call_user_func(self::$authUsing, $request);
+        }
+
+        return app()->environment('local');
+    }
+}
